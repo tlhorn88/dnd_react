249175abc71bb022a5d6d76dfdf263754d0f6c49
Tabetha Horn <tabethahorn@Tabethas-MBP.attlocal.net>
2023-05-06 20:11:11 -0500

    removed images to make netlify happy

--- src/Components/TestPages/Page3.js
+++ src/Components/TestPages/Page3.js
@@ -2,7 +2,7 @@ function Page3() {
   return (
     <div>
       <p>page 3</p>
-      <img src={require('../../img/puppies3.jpeg')} />
+      {/* <img src={require('../../img/puppies3.jpeg')} /> */}
     </div>
   );
 }
