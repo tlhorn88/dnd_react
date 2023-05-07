b8b1bcc88851cebb04199178e2a979cd8f6bfe93
Tabetha Horn <tabethahorn@Tabethas-MBP.attlocal.net>
2023-05-06 20:44:12 -0500

    artificially added an error to test netlify features

--- src/Components/TestPages/Page1.js
+++ src/Components/TestPages/Page1.js
@@ -2,7 +2,7 @@ function Page1() {
   return (
     <div>
       <p>page 1</p>
-      <img alt="" src={require('../../img/puppies1.avif')} />
+      <img src={require('../../img/puppies1.avif')} />
     </div>
   )
 }
